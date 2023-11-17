b24ed7eefdf2abaef26f0c1fa551283c6be17854
Bert De Saffel <bert.desaffel@4dvision.be>
2023-11-17 23:00:07 +0100

    Add project files.

---
+++ SuperWordle/Models/WordleWord.cs
@@ -0,0 +1,24 @@
+﻿namespace SuperWordle.Models {
+    public class WordleWord {
+
+        public WordleWord(string word) {
+            Characters = word.ToCharArray();
+        }
+
+
+        private char[] _characters;
+        public char[] Characters {
+            get {
+                return _characters;
+            }
+            set {
+                if (value.Length != 5) {
+                    throw new ArgumentException("A wordle word must contain exactly 5 characters.");
+                }
+                _characters = value;
+            }
+        }
+
+
+    }
+}
